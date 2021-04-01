a85df7a0ded71e3ec37be75509db36a49fda96e4
jamestodd313 <jamestodd313@gmail.com>
2021-04-01 16:31:43 -0500

    added dynamic views to navbar with placeholder auth state

--- client/src/components/common/Nav.js
+++ client/src/components/common/Nav.js
@@ -4,13 +4,23 @@ import {Menu} from 'semantic-ui-react'
 
 export const Nav = ({active}) => {
     const [activeLink, setActiveLink] = useState(active)
+    const authenticated = true
     return (
         <Menu header>
             <Menu.Item header>APPPPPPPP</Menu.Item>
             <Menu.Menu position="right">
                 <Menu.Item name="home" active={activeLink === "home"} onClick={e=> setActiveLink('home')} as={Link} to="/"/>
-                <Menu.Item name="logIn" active={activeLink === "logIn"} onClick={e=> setActiveLink('logIn')} as={Link} to="/login"/>
-                <Menu.Item name="signUp" active={activeLink === "signUp"} onClick={e=> setActiveLink('signUp')} as={Link} to="/register"/>
+                {
+                    authenticated ? (
+                        <Menu.Item name="signOut" active={activeLink === "signOut"} onClick={e=> setActiveLink('signOut')} as={Link} to="/"/>
+
+                    ) : (
+                        <>
+                            <Menu.Item name="logIn" active={activeLink === "logIn"} onClick={e=> setActiveLink('logIn')} as={Link} to="/login"/>
+                            <Menu.Item name="signUp" active={activeLink === "signUp"} onClick={e=> setActiveLink('signUp')} as={Link} to="/register"/>
+                        </>
+                    )
+                }
             </Menu.Menu>
 
         </Menu>
